d31af5c450b3556fb0207c7c559889f0c3eb8847
Jens <jns.bys@gmail.com>
2021-03-23 12:10:55 +0100

    setup Loaders and calculation Model

--- Controller/CustomerController.php
+++ Controller/CustomerController.php
@@ -3,5 +3,14 @@
 
 class CustomerController
 {
+private PDO $pdo;
 
+public function __construct(){
+    $connection = new DataBase();
+    $this->pdo = $connection->connect();
+}
+
+public function getCustomerDetails(){
+
+}
 }
