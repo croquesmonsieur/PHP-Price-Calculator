80553dd17e765e0d5fbe584754a49b3158586b48
serkan <s.safran84@gmail.com>
2021-03-24 11:39:08 +0100

    dropdown menu

--- Controller/ProductsController.php
+++ Controller/ProductsController.php
@@ -14,6 +14,7 @@ class ProductsController
     public function getProductList()
     {
         $products = ProductsLoader::getProducts($this->pdo);
+        require "View/CustomerView.php";
     }
 
 }
